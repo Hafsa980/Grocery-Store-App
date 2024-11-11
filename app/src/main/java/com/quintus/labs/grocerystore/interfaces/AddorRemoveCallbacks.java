fc7bb2a7a86c04bbaf6c5d4a4bb6d51447f77d9f
Hafsa Fayyaz <hafsafayyaz980@gmail.com>
2024-11-11 14:15:26 +0500

    Grocery Store

---
+++ app/src/main/java/com/quintus/labs/grocerystore/interfaces/AddorRemoveCallbacks.java
@@ -0,0 +1,10 @@
+package com.quintus.labs.grocerystore.interfaces;
+
+public interface AddorRemoveCallbacks {
+
+    void onAddProduct();
+
+    void onRemoveProduct();
+
+    void updateTotalPrice();
+}
